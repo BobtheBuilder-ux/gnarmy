c7f7c755c24c17c8833e80ca0fc28464daba95fb
BobtheBuilder-ux <admin@homematch.ng>
2025-11-15 18:25:37 +0100

    hrer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -115,9 +115,9 @@ export function Footer() {
             <p className="text-white/60 text-sm">
               © {new Date().getFullYear()} Gnarmy Coal Trade and Consultancy Ltd. All rights reserved.
             </p>
-            <p className="text-white/60 text-sm">
+            {/* <p className="text-white/60 text-sm">
               100% Indigenous Nigerian Company
-            </p>
+            </p> */}
             <p className="text-white/60 text-sm text-center md:text-left">
               Ajoo Management and Consultancy Ltd (Subsidiary of GNARMY COAL TRADE AND CONSULTANCY LTD)
             </p>
